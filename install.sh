020beb431a6ca85ce7d6b9f1f3b662fdd34f5a8b
Filterrr <36611101+Filterrr@users.noreply.github.com>
2021-06-29 01:05:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -469,6 +469,7 @@ port_exist_check() {
     fi
 }
 acme() {
+    "$HOME"/.acme.sh/acme.sh --set-default-ca --server letsencrypt
     if "$HOME"/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force --test; then
         echo -e "${OK} ${GreenBG} SSL 证书测试签发成功，开始正式签发 ${Font}"
         rm -rf "$HOME/.acme.sh/${domain}_ecc"
